5160fa1940b8612b858cd6f00b955eea5dec6e6d
matthias <schmid_mat@gmx.at>
2011-10-27 09:57:01 +0200

    added Scheduler

---
+++ src/ottos/kernel/sched/Scheduler.cc
@@ -0,0 +1,43 @@
+/* Scheduler.cpp
+ * 
+ * Copyright (c) 2011 The ottos project.
+ *
+ * This work is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ * 
+ * This work is distributed in the hope that it will be useful, but without
+ * any warranty; without even the implied warranty of merchantability or
+ * fitness for a particular purpose. See the GNU Lesser General Public License
+ * for more details.
+ *s
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation,
+ * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ *
+ *  Created on: Oct 21, 2011
+ *      Author: Matthias Schmid <m.schmid@students.fhv.at>
+ */
+
+#include "Scheduler.h"
+#include "../pm/Process.h"
+#include "RoundRobin.h"
+
+Scheduler::Scheduler(RoundRobin algo) {
+  current_ = -1;            /* no proc has started yet; current_ is -1 */
+  algorithm_ = algo;
+}
+
+Scheduler::~Scheduler() {
+}
+
+int Scheduler::next(Process** procs, int size) {
+  current_ = algorithm_.next(procs, current_, size);
+  return current_;
+}
+
+int Scheduler::current(void) {
+  return current_;
+}
