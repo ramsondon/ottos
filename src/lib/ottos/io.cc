f89584eaf17a8ab7c452ab212851f83df02add05
Franziskus Domig <fdomig@gmail.com>
2011-11-04 13:31:06 +0100

    added IO lib

---
+++ src/lib/ottos/io.cc
@@ -0,0 +1,40 @@
+/* io.cc
+ * 
+ * Copyright (c) 2011 The ottos project.
+ *
+ * This work is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ * 
+ * This work is distributed in the hope that it will be useful, but without
+ * any warranty; without even the implied warranty of merchantability or
+ * fitness for a particular purpose. See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation,
+ * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ *
+ *  Created on: 04.11.2011
+ *      Author: Franziskus Domig <fdomig@gmail.com>
+ */
+
+#include <ottos/io.h>
+#include <ottos/types.h>
+#include <ottos/system.h>
+
+file_t* fopen(char* filename, char* mode) {
+  int flags = 0;
+  file_t* fp = (file_t *) sys_open(filename, flags);
+  return fp;
+}
+
+int fclose(file_t* fp) {
+  return 0;
+}
+
+int fwrite(file_t* fp, size_t size, size_t count, char* buffer) {
+  return 0;
+}
