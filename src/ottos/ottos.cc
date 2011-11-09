7f32e204b7916750d262355c128ff3d0c8e5fa62
matthias <schmid_mat@gmx.at>
2011-11-09 17:19:59 +0100

    deleted project files2

--- src/ottos/ottos.cc
+++ src/ottos/ottos.cc
@@ -25,13 +25,85 @@
 
 #include "../../bin/led_test.h"
 #include "kernel/intc/InterruptHandler.h"
+#include "kernel/timer/GPTimerController.h"
 #include "kernel/pm/Process_c.h"
 
+#include <stdio.h>
+
+Scheduler* sched;
+
+#define GPIO5_OE 0x49054034       /* GPIO5 output enable address */
+#define GPIO5_DATAOUT 0x4905603C  /* GPIO5 data out register address*/
+
+#define SET_BIT(bit) (1<<bit)     /* Sets the specified bit to 1 */
+
+void toggle_led_1() {
+  volatile int i;
+  //printf("Timer 3 fired interrupt... \n");
+
+  GPTimerController gptc;
+  gptc.clear(3);
+
+  *(volatile unsigned long *)GPIO5_DATAOUT ^= SET_BIT(22);
+
+  gptc.reset(3);
+}
+
+void toggle_led_2() {
+  volatile int i;
+  //printf("Timer 4 fired interrupt... \n");
+
+  GPTimerController gptc;
+  gptc.clear(4);
+
+  *(volatile unsigned long *)GPIO5_DATAOUT ^= SET_BIT(21);
+
+  gptc.reset(4);
+}
+
+
 int main(int argc, char **argv) {
+  /*
+  Kernel k = Kernel();
+  k.init();
+  k.run();
+  */
+
+  // set up gptimer 1
+  InterruptController ic = InterruptController();
+  ic.add_handler(39, toggle_led_1);
+  ic.add_handler(40, toggle_led_2);
+
+  printf("init handler... \n");
+
+  GPTimerController gptController;
+  gptController.init(3, 1000000);
+  gptController.start(3, MATCH_INTERRUPT);
+  printf("init GPT2... \n");
+
+  gptController.init(4, 1500000);
+  gptController.start(4, MATCH_INTERRUPT);
+  printf("init GPT3... \n");
+
+  // enable IRQs
+  _enable_interrupts();
+
+  gptController.reset(3);
+  gptController.reset(4);
+
+  while (true) {}
+
+  return 0;
+}
+
+
+int main_(int argc, char **argv) {
 
 //	kernel = new Kernel();
 //	kernel->init();
 //	kernel->run();
+
+
 
 	started = FALSE;
 
