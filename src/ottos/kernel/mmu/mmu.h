05447a084470c247eee0132b6795fd2556a385aa
Thomas Wiedemann <thomas.wiedemann@students.fhv.at>
2011-11-14 10:26:58 +0100

    Modified MMU

--- src/ottos/kernel/mmu/mmu.h
+++ src/ottos/kernel/mmu/mmu.h
@@ -8,6 +8,7 @@
 #ifndef MMU_H_
 #define MMU_H_
 
+#include <stdint.h>
 
 #define ENABLEMMU       0x00000001
 #define ENABLEDCACHE    0x00000004
@@ -18,6 +19,18 @@
 #define CHANGEICACHE    0x00001000
 
 //To do: Struct der Masterpagetable
+typedef struct {
+    uint8_t PTE; //Pagetableentry
+    uint8_t PTn;     //pagetablenummer
+
+}masterpagetable_t;
+
+typedef struct {
+    uint8_t PTE;  //Pagetableentry
+
+}l2pagetable_t;
+
+
 
 
 //Prototype
