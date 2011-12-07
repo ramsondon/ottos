1885f46ea597de947776bad539d23abd21b0d829
matthias <schmid_mat@gmx.at>
2011-12-07 10:47:05 +0100

    dir struct refactoring

---
+++ src/hal/platform.h
@@ -0,0 +1,42 @@
+/* platform.h
+ * 
+ * Copyright (c) 2011 The ottos project.
+ *
+ * This work is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ * 
+ * This work is distributed in the hope that it will be useful, but without
+ * any warranty; without even the implied warranty of merchantability or
+ * fitness for a particular purpose. See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation,
+ * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ *
+ *  Created on: Dec 2, 2011
+ *      Author: Matthias Schmid <ramsondon@gmail.com>
+ */
+
+#ifndef HAL_PLATFORM_H_
+#define HAL_PLATFORM_H_
+
+/* *****************************************************************************
+ * XXX: TO NOTIFY BY THE PROGRAMMER
+ * *****************************************************************************
+ *
+ * This is the main include file for the OttOS hardware abstraction layer.
+ * All modules which require one part of the OttOS HAL must only include this
+ * header file
+ */
+
+/*
+ * TODO(ramsondon@gmail.com): check with ifdef OMAP353X, then include
+ */
+#include "omap353x/platform.h"
+
+
+#endif /* HAL_H_ */
