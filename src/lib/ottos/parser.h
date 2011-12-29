4fcae61b552e8d42c9d391a32b83306d3e00245b
unknown <Thomas@Anarxo.(none)>
2011-12-29 18:31:32 +0100

    changed copyright to honour wolfy42

--- src/lib/ottos/parser.h
+++ src/lib/ottos/parser.h
@@ -1,6 +1,6 @@
 /* parser.h
  *
- * Copyright (c) 2011 The ottos project.
+ * Copyright (c) 2011 The ottos project. Inspired by the BOSS project.
  *
  * This work is free software; you can redistribute it and/or modify it under
  * the terms of the GNU Lesser General Public License as published by the Free
