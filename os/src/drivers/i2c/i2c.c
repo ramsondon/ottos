d34b22d8b8841171fe39677ea1223534ba20919d
Matthias Schmid <schmid_mat@gmx.at>
2012-03-09 15:41:30 +0100

    Update os/src/drivers/i2c/i2c.c

--- os/src/drivers/i2c/i2c.c
+++ os/src/drivers/i2c/i2c.c
@@ -1,26 +1,6 @@
-/* i2c.c
- * 
- * Copyright (c) 2011 The ottos project.
- *
- * This work is free software; you can redistribute it and/or modify it under
- * the terms of the GNU Lesser General Public License as published by the Free
- * Software Foundation; either version 2.1 of the License, or (at your option)
- * any later version.
- * 
- * This work is distributed in the hope that it will be useful, but without
- * any warranty; without even the implied warranty of merchantability or
- * fitness for a particular purpose. See the GNU Lesser General Public License
- * for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this library; if not, write to the Free Software Foundation,
- * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
- *
- *
- *  Created on: 16 Dec 2011
- *      Author: Thomas Bargetz <thomas.bargetz@gmail.com>
- */
-
+/*
+* puppybits code
+*/
 #include <ottos/types.h>
 #include <ottos/kernel.h>
 #include <ottos/memory.h>
